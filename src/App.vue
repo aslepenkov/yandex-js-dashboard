9836d8c2e592ddcbe3aa0889d734f7665f090830
Alexey Slepenkov <aslepenkov@xtools.pro>
2019-08-21 20:38:15 +0700

    first result

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div id="app">
-    <div v-if="loading">Loading...</div>
-
+    <div v-if="loading">
+      <div class="lds-hourglass"></div>Loading... прокси медленный, ждать почти минуту :)
+    </div>
     <b-table striped hover :items="items"></b-table>
-
-    <pre>players: {{players}}</pre>
-    <pre>challengeSlugs: {{challengeSlugs}}</pre>
-    <pre>posts: {{posts}}</pre>
   </div>
 </template>
 
@@ -17,15 +14,31 @@ export default {
   name: "app",
   data() {
     return {
-      posts: [],
+      results: [],
       items: [],
       loading: true,
       players: [],
       challengeSlugs: []
     };
   },
+  async beforeMount() {
+    await this.fetchTableData(true);
+  },
   async created() {
     await this.fetchTableData();
+
+    this.items.sort(function(a, b) {
+      var adoneCount = a.doneCount;
+      var bdoneCount = b.doneCount;
+      var aLow = a.diffTime;
+      var bLow = b.diffTime;
+
+      if (adoneCount === bdoneCount) {
+        return aLow < bLow ? -1 : aLow > bLow ? 1 : 0;
+      } else {
+        return adoneCount < bdoneCount ? 1 : -1;
+      }
+    });
   },
   methods: {
     async getPlayersDataAsync() {
@@ -33,7 +46,8 @@ export default {
         let data = [];
         //load players
         let playersUrl =
-          "https://private-8fa4d2-xtstat.apiary-mock.com/players";
+          "https://yandex-js-dashboard-api.firebaseapp.com/players";
+        //"https://private-8fa4d2-xtstat.apiary-mock.com/players";
 
         await axios
           .get(playersUrl)
@@ -56,7 +70,7 @@ export default {
       try {
         let data = [];
         //load challenges
-        let slugsUrl = "https://private-8fa4d2-xtstat.apiary-mock.com/slugs";
+        let slugsUrl = "https://yandex-js-dashboard-api.firebaseapp.com/slugs";
         await axios
           .get(slugsUrl)
           .then(response => {
@@ -74,12 +88,17 @@ export default {
         console.log(error);
       }
     },
-    async getPostsAsync() {
+    async getResultsAsync(headersOnly) {
       try {
         let data = [];
-
+        let secret = "WbsyyvpERFjV9csHcv_7";
+        let run_count = headersOnly ? 1 : -500; //magic number!
         for (let player of this.players) {
-          let url = "https://private-8fa4d2-xtstat.apiary-mock.com/"; //TODO 'https://www.codewars.com/api/v1/users/'+player+'/code-challenges/completed?access_key=TEST_KEY'
+          if (run_count++ > 1) continue;
+          let url =
+            "https://cors-anywhere.herokuapp.com/" + //TODO own server
+            `www.codewars.com/api/v1/users/${player}/code-challenges/completed?access_key=${secret}`;
+
           await axios
             .get(url)
             .then(response => {
@@ -87,7 +106,21 @@ export default {
                 val["player"] = player;
                 val["time"] = val.completedAt;
 
-                data.push(val);
+                if (this.challengeSlugs.includes(val.slug)) {
+                  data.push({
+                    player: player,
+                    slug: val.slug,
+                    completedAt: val.completedAt,
+                    done: "+"
+                  });
+                } else {
+                  data.push({
+                    player: player,
+                    slug: val.slug,
+                    completedAt: "",
+                    done: "-"
+                  });
+                }
               });
             })
             .then(resp => {})
@@ -102,40 +135,70 @@ export default {
         console.log(error);
       }
     },
-    async fetchTableData() {
+    async fetchTableData(first) {
       this.players = await this.getPlayersDataAsync();
       this.challengeSlugs = await this.getSlugsDataAsync();
-      this.posts = await this.getPostsAsync();
+      this.results = await this.getResultsAsync(first);
 
-      this.items = this.players.map(player => {
+      this.items = await this.players.map(player => {
         let row = {};
         row.playerName = player;
+        row.doneCount = 0;
+        row.diffTime = 0;
 
         for (let slug of this.challengeSlugs) {
-          let timestr = this.posts
+          let timestr = this.results
             .filter(res => {
               return res.player === player && res.slug === slug;
             })
             .map(s => s.completedAt)[0];
 
-          // let timestr = new Date(
-          //   resulTime.getTime() - resulTime.getTimezoneOffset() * 60000
-          // )F
-          //   .toISOString()F
-          //   .replace("T", " ")
-          //   .slice(0, 16);
+          if (!first) {
+            let resultTime = new Date(timestr);
 
-          let resultTime = new Date(timestr);
+            let fastestTime = this.results
+              .filter(res => {
+                return res.slug === slug;
+              })
+              .map(s => new Date(s.completedAt))
+              .filter(d => {
+                return d instanceof Date && isFinite(d);
+              })
+              .sort((a, b) => {
+                if (+a.getTime() > +b.getTime()) return 1;
+                if (+a.getTime() < +b.getTime()) return -1;
+                return 0;
+              })[0];
 
-          if (resultTime instanceof Date && isFinite(resultTime)) {
-            row[slug] = resultTime;
-          } else {
-            row[slug] = "-";
+            if (resultTime instanceof Date && isFinite(resultTime)) {
+              row.doneCount++;
+              if (fastestTime.getTime() === resultTime.getTime()) {
+                row[slug] = "+";
+                //row._cellVariants = { [slug]: "success" };
+              } else {
+                row[slug] = "+";
+                row.diffTime += Math.abs(
+                  resultTime.getTime() - fastestTime.getTime()
+                );
+              }
+              // row[slug] =
+              //   "+" +
+              //   Math.floor(
+              //     Math.abs(resultTime.getTime() - fastestTime.getTime()) /
+              //       60000 /
+              //       60
+              //   ) +
+              //   " hrs";
+            } else {
+              row[slug] = "-";
+            }
           }
         }
+
         return row;
       });
-      this.loading = false;
+
+      if (!first) this.loading = false;
     }
   }
 };
@@ -156,17 +219,41 @@ h2 {
   font-weight: normal;
 }
 
-/* ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-} */
-
 a {
   color: #42b983;
+}
+
+.lds-hourglass {
+  width: 30%;
+  height: 30%;
+  display: block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-hourglass:after {
+  content: " ";
+  display: block;
+  border-radius: 50%;
+  width: 0;
+  height: 0;
+  margin: 6px;
+  box-sizing: border-box;
+  border: 26px solid rgb(97, 97, 97);
+  border-color: rgb(97, 97, 97) transparent rgb(97, 97, 97) transparent;
+  animation: lds-hourglass 1.2s infinite;
+}
+@keyframes lds-hourglass {
+  0% {
+    transform: rotate(0);
+    animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+  50% {
+    transform: rotate(900deg);
+    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+  }
+  100% {
+    transform: rotate(1800deg);
+  }
 }
 </style>
